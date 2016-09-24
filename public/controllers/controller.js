001cf146576b9246b6f1de80109c5632af0863eb
Dylan Basdeo <d_basdeo@hotmail.com>
2016-09-24 12:18:00 -0700

    Map addded to homepage

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -94,5 +94,41 @@ $scope.deselect = function() {
 }
 
 
+ var mapOptions = {
+                  zoom: 8,
+                  center: new google.maps.LatLng(49, -123),
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+              }
+
+              $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+              $scope.markers = [];
+              
+              var infoWindow = new google.maps.InfoWindow();
+              
+              var createMarker = function (info){
+                  
+                  var marker = new google.maps.Marker({
+                      map: $scope.map,
+                      position: new google.maps.LatLng(info.lat, info.long),
+                      title: info.place
+                  });
+                  marker.content = '<div class="infoWindowContent">' + info.desc + '<br />' + info.lat + ' E,' + info.long +  ' N, </div>';
+                  
+                  google.maps.event.addListener(marker, 'click', function(){
+                      infoWindow.setContent('<h2>' + marker.title + '</h2>' + 
+                        marker.content);
+                      infoWindow.open($scope.map, marker);
+                  });
+                  
+                  $scope.markers.push(marker);
+                  
+              }  
+              
+
+              $scope.openInfoWindow = function(e, selectedMarker){
+                  e.preventDefault();
+                  google.maps.event.trigger(selectedMarker, 'click');
+              }
 
 }]);﻿
