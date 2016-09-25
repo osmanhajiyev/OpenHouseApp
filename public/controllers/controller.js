eec992be940767375ac046ce0e4c0530b0cb5681
Dylan Basdeo <d_basdeo@hotmail.com>
2016-09-25 11:21:21 -0700

    Charts appear

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -148,6 +148,38 @@ $scope.updateTextInput = function(val) {
 
 
 myApp.controller('mapCtrl', ['$scope', '$http', '$rootScope', '$location', function($scope, $http, $rootScope, $location) {
+        $scope.config22 = {
+    title: 'Products',
+    tooltips: false,
+    labels: false,
+    mouseover: function() {},
+    mouseout: function() {},
+    click: function() {},
+    legend: {
+      display: true,
+      //could be 'left, right'
+      position: 'right'
+    }
+  };
+
+  $scope.data22 = {
+    series: ['Cost per bdrm'],
+    data: [{
+      x: "1 bdrm",
+      y: [100],
+    }, {
+      x: "2 bdrm",
+      y: [300]
+    }, {
+      x: "3 bdrm",
+      y: [351]
+    }, {
+      x: "4 brdm",
+      y: [54]
+    }]
+  };
+
+
     var mapOptions = {
         zoom: 12,
         center: new google.maps.LatLng(49.2827, -123.116226),
@@ -199,40 +231,78 @@ myApp.controller('mapCtrl', ['$scope', '$http', '$rootScope', '$location', funct
         infoWindow.open($scope.map);
     }
 
-        $scope.data = [{
-                    label: "Bakersfield Central",
-                    value: "880000"
-                },
-                {
-                    label: "Garden Groove harbour",
-                    value: "730000"
-                },
-                {
-                    label: "Los Angeles Topanga",
-                    value: "590000"
-                },
-                {
-                    label: "Compton-Rancho Dom",
-                    value: "520000"
-                },
-                {
-                    label: "Daly City Serramonte",
-                    value: "330000"
-                }];
-    $scope.config = {
-  title: '',
-  tooltips: true,
-  labels: false,
-  mouseover: function() {},
-  mouseout: function() {},
-  click: function() {},
-  legend: {
-    display: true,
-    //could be 'left, right'
-    position: 'left'
-  },
-  innerRadius: 0, // applicable on pieCharts, can be a percentage like '50%'
-  lineLegend: 'lineEnd' // can be also 'traditional'
-} 
-
 }]);
+
+myApp.directive('ngDraggable', function($document) {
+  return {
+    restrict: 'A',
+    scope: {
+      dragOptions: '=ngDraggable'
+    },
+    link: function(scope, elem, attr) {
+      var startX, startY, x = 0, y = 0,
+          start, stop, drag, container;
+
+      var width  = elem[0].offsetWidth,
+          height = elem[0].offsetHeight;
+
+      // Obtain drag options
+      if (scope.dragOptions) {
+        start  = scope.dragOptions.start;
+        drag   = scope.dragOptions.drag;
+        stop   = scope.dragOptions.stop;
+        var id = scope.dragOptions.container;
+        if (id) {
+            container = document.getElementById(id).getBoundingClientRect();
+        }
+      }
+
+      // Bind mousedown event
+      elem.on('mousedown', function(e) {
+        e.preventDefault();
+        startX = e.clientX - elem[0].offsetLeft;
+        startY = e.clientY - elem[0].offsetTop;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+        if (start) start(e);
+      });
+
+      // Handle drag event
+      function mousemove(e) {
+        y = e.clientY - startY;
+        x = e.clientX - startX;
+        setPosition();
+        if (drag) drag(e);
+      }
+
+      // Unbind drag events
+      function mouseup(e) {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+        if (stop) stop(e);
+      }
+
+      // Move element, within container if provided
+      function setPosition() {
+        if (container) {
+          if (x < container.left) {
+            x = container.left;
+          } else if (x > container.right - width) {
+            x = container.right - width;
+          }
+          if (y < container.top) {
+            y = container.top;
+          } else if (y > container.bottom - height) {
+            y = container.bottom - height;
+          }
+        }
+
+        elem.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+    }
+  }
+
+});
